77ad27a6f8267b069b0562cb6373e0df74de09ec
Za-Sing <za.sing227@gmail.com>
2021-03-13 16:34:48 -0600

    Mostly finished Card class, should probably make a constructor

--- src/Card.h
+++ src/Card.h
@@ -4,29 +4,20 @@
 
 class Card
 {
-private:
-	typedef const enum {Alaska, Northwest_Territory, Greenland, Alberta, Ontario, Eastern_Canada,
-					Western_United_States, Eastern_United_States, Central_America, Venezuela, Brazil, Peru,
-					Argentina, Iceland, Scandinavia, Russia, Great_Britain, Northern_Europe,
-					Western_Europe, Southern_Europe, Ural, Siberia, Yakutsk, Irkutsk, Kamchatka,
-					Afghanistan, China, Mongolia, Japan, Middle_East, India, Southeast_Asia,
-					North_Africa, Egypt, East_Africa, Central_Africa, South_Africa, Madagascar,
-					Indonesia, New_Guinea, Western_Australia, Eastern_Australia} TERRITORY;
-	
-	typedef const enum {Infantry=1, Cavalry=5, Artillery=10} TROOP;
-	
-	/*const vector<string> TERRITORIES = {"Alaska", "Northwest Territory", "Greenland", "Alberta", "Ontario", "Eastern Canada", 
-										"Western United States", "Eastern United States", "Central America", "Venezuela", "Brazil", "Peru",
-										"Argentina", "Iceland", "Scandinavia", "Russia", "Great Britain", "Northern Europe", 
-										"Western Europe", "Southern Europe", "Ural", "Siberia", "Yakutsk", "Irkutsk", "Kamchatka", 
-										"Afghanistan", "China", "Mongolia", "Japan", "Middle East", "India", "Southeast Asia",
-										"North Africa", "Egypt", "East Africa", "Central Africa", "South Africa", "Madagascar", 
-										"Indonesia", "New Guinea", "Western Australia", "Eastern Australia"};
-	const vector<int> TROOPS = {1, 5, 10};*/
-	
-public:  
+public:
+	enum class TERRITORY {
+		Alaska, Northwest_Territory, Greenland, Alberta, Ontario, Eastern_Canada,
+		Western_United_States, Eastern_United_States, Central_America, Venezuela, Brazil, Peru,
+		Argentina, Iceland, Scandinavia, Russia, Great_Britain, Northern_Europe,
+		Western_Europe, Southern_Europe, Ural, Siberia, Yakutsk, Irkutsk, Kamchatka,
+		Afghanistan, China, Mongolia, Japan, Middle_East, India, Southeast_Asia,
+		North_Africa, Egypt, East_Africa, Central_Africa, South_Africa, Madagascar,
+		Indonesia, New_Guinea, Western_Australia, Eastern_Australia
+	};
+
+	enum class TROOP { Infantry = 1, Cavalry = 5, Artillery = 10 };
+
 	TERRITORY territory;
 	TROOP troop;
-
 };
 
