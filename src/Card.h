737795ba4b45b1059f2301e8d8759d40f55819d9
Za-Sing <za.sing227@gmail.com>
2021-03-14 15:52:20 -0500

    Finalized, changed to a struct for easier use

--- src/Card.h
+++ src/Card.h
@@ -1,22 +1,22 @@
 #pragma once
-#include <vector>
-#include <string>
 
-class Card
+// Enumeration for all possible territories
+typedef enum TERRITORY {
+	Alaska, Northwest_Territory, Greenland, Alberta, Ontario, Eastern_Canada,
+	Western_United_States, Eastern_United_States, Central_America, Venezuela, Brazil, Peru,
+	Argentina, Iceland, Scandinavia, Russia, Great_Britain, Northern_Europe,
+	Western_Europe, Southern_Europe, Ural, Siberia, Yakutsk, Irkutsk, Kamchatka,
+	Afghanistan, China, Mongolia, Japan, Middle_East, India, Southeast_Asia,
+	North_Africa, Egypt, East_Africa, Central_Africa, South_Africa, Madagascar,
+	Indonesia, New_Guinea, Western_Australia, Eastern_Australia
+};
+
+// Enumeration for all troop names & values
+typedef enum TROOP { Infantry = 1, Cavalry = 5, Artillery = 10 };
+
+// Simple struct for the card's values
+typedef struct Card
 {
-public:
-	enum class TERRITORY {
-		Alaska, Northwest_Territory, Greenland, Alberta, Ontario, Eastern_Canada,
-		Western_United_States, Eastern_United_States, Central_America, Venezuela, Brazil, Peru,
-		Argentina, Iceland, Scandinavia, Russia, Great_Britain, Northern_Europe,
-		Western_Europe, Southern_Europe, Ural, Siberia, Yakutsk, Irkutsk, Kamchatka,
-		Afghanistan, China, Mongolia, Japan, Middle_East, India, Southeast_Asia,
-		North_Africa, Egypt, East_Africa, Central_Africa, South_Africa, Madagascar,
-		Indonesia, New_Guinea, Western_Australia, Eastern_Australia
-	};
-
-	enum class TROOP { Infantry = 1, Cavalry = 5, Artillery = 10 };
-
 	TERRITORY territory;
 	TROOP troop;
 };
