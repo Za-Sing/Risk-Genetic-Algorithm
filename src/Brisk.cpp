23a4c01808d2b44b813d758502e4ecf93daafc1e
thetateman <tdsmith2018@gmail.com>
2021-03-27 13:23:01 -0500

    more countries

--- src/Brisk.cpp
+++ src/Brisk.cpp
@@ -43,6 +43,41 @@ Brisk::Brisk()
 	board.push_back(Region(6, "Western_United_States", vector<int>{3, 4, 7, 8}));
 	board.push_back(Region(7, "Eastern_United_States", vector<int>{6, 4, 5, 8}));
 	board.push_back(Region(8, "Central_America", vector<int>{6, 7}));
+	board.push_back(Region(9, "Venezuela", vector<int>{})); 
+	board.push_back(Region(10, "Brazil", vector<int>{})); 
+	board.push_back(Region(11, "Peru", vector<int>{}));
+	board.push_back(Region(12, "Argentina", vector<int>{})); 
+	board.push_back(Region(13, "Iceland", vector<int>{})); 
+	board.push_back(Region(14, "Scandinavia", vector<int>{})); 
+	board.push_back(Region(15, "Russia", vector<int>{})); 
+	board.push_back(Region(16, "Great_Britain", vector<int>{})); 
+	board.push_back(Region(17, "Northern_Europe", vector<int>{}));
+	board.push_back(Region(18, "Western_Europe", vector<int>{})); 
+	board.push_back(Region(19, "Southern_Europe", vector<int>{})); 
+	board.push_back(Region(20, "Ural", vector<int>{})); 
+	board.push_back(Region(21, "Siberia", vector<int>{})); 
+	board.push_back(Region(22, "Yakutsk", vector<int>{})); 
+	board.push_back(Region(23, "Irkutsk", vector<int>{})); 
+	board.push_back(Region(24, "Kamchatka", vector<int>{}));
+	board.push_back(Region(25, "Afghanistan", vector<int>{})); 
+	board.push_back(Region(26, "China", vector<int>{})); 
+	board.push_back(Region(27, "Mongolia", vector<int>{})); 
+	board.push_back(Region(28, "Japan", vector<int>{})); 
+	board.push_back(Region(29, "Middle_East", vector<int>{})); 
+	board.push_back(Region(30, "India", vector<int>{})); 
+	board.push_back(Region(31, "Southeast_Asia", vector<int>{}));
+	board.push_back(Region(32, "North_Africa", vector<int>{})); 
+	board.push_back(Region(33, "Egypt", vector<int>{})); 
+	board.push_back(Region(34, "East_Africa", vector<int>{})); 
+	board.push_back(Region(35, "Central_Africa", vector<int>{})); 
+	board.push_back(Region(36, "South_Africa", vector<int>{})); 
+	board.push_back(Region(37, "Madagascar", vector<int>{}));
+	board.push_back(Region(38, "Indonesia", vector<int>{})); 
+	board.push_back(Region(39, "New_Guinea", vector<int>{})); 
+	board.push_back(Region(40, "Western_Australia", vector<int>{})); 
+	board.push_back(Region(41, "Eastern_Australia", vector<int>{}));
+
+
 
 }
 
