a3f34738675b384ec5c1ff4cf9a69c4de4b460fc
Za-Sing <za.sing227@gmail.com>
2021-03-27 17:29:05 -0500

    Completed border assignment, removed 2-player option, added initDeck to constructor and added const numregions

--- src/Brisk.cpp
+++ src/Brisk.cpp
@@ -43,42 +43,42 @@ Brisk::Brisk()
 	board.push_back(Region(6, "Western_United_States", vector<int>{3, 4, 7, 8}));
 	board.push_back(Region(7, "Eastern_United_States", vector<int>{6, 4, 5, 8}));
 	board.push_back(Region(8, "Central_America", vector<int>{6, 7}));
-	board.push_back(Region(9, "Venezuela", vector<int>{})); 
-	board.push_back(Region(10, "Brazil", vector<int>{})); 
-	board.push_back(Region(11, "Peru", vector<int>{}));
-	board.push_back(Region(12, "Argentina", vector<int>{})); 
-	board.push_back(Region(13, "Iceland", vector<int>{})); 
-	board.push_back(Region(14, "Scandinavia", vector<int>{})); 
-	board.push_back(Region(15, "Russia", vector<int>{})); 
-	board.push_back(Region(16, "Great_Britain", vector<int>{})); 
-	board.push_back(Region(17, "Northern_Europe", vector<int>{}));
-	board.push_back(Region(18, "Western_Europe", vector<int>{})); 
-	board.push_back(Region(19, "Southern_Europe", vector<int>{})); 
-	board.push_back(Region(20, "Ural", vector<int>{})); 
-	board.push_back(Region(21, "Siberia", vector<int>{})); 
-	board.push_back(Region(22, "Yakutsk", vector<int>{})); 
-	board.push_back(Region(23, "Irkutsk", vector<int>{})); 
-	board.push_back(Region(24, "Kamchatka", vector<int>{}));
-	board.push_back(Region(25, "Afghanistan", vector<int>{})); 
-	board.push_back(Region(26, "China", vector<int>{})); 
-	board.push_back(Region(27, "Mongolia", vector<int>{})); 
-	board.push_back(Region(28, "Japan", vector<int>{})); 
-	board.push_back(Region(29, "Middle_East", vector<int>{})); 
-	board.push_back(Region(30, "India", vector<int>{})); 
-	board.push_back(Region(31, "Southeast_Asia", vector<int>{}));
-	board.push_back(Region(32, "North_Africa", vector<int>{})); 
-	board.push_back(Region(33, "Egypt", vector<int>{})); 
-	board.push_back(Region(34, "East_Africa", vector<int>{})); 
-	board.push_back(Region(35, "Central_Africa", vector<int>{})); 
-	board.push_back(Region(36, "South_Africa", vector<int>{})); 
-	board.push_back(Region(37, "Madagascar", vector<int>{}));
-	board.push_back(Region(38, "Indonesia", vector<int>{})); 
-	board.push_back(Region(39, "New_Guinea", vector<int>{})); 
-	board.push_back(Region(40, "Western_Australia", vector<int>{})); 
-	board.push_back(Region(41, "Eastern_Australia", vector<int>{}));
+	board.push_back(Region(9, "Venezuela", vector<int>{8, 10, 11})); 
+	board.push_back(Region(10, "Brazil", vector<int>{9, 11, 12})); 
+	board.push_back(Region(11, "Peru", vector<int>{9, 10, 12}));
+	board.push_back(Region(12, "Argentina", vector<int>{10, 11})); 
+	board.push_back(Region(13, "Iceland", vector<int>{2, 14, 16})); 
+	board.push_back(Region(14, "Scandinavia", vector<int>{13, 15, 16, 17})); 
+	board.push_back(Region(15, "Russia", vector<int>{14, 17, 19, 20, 25, 29})); 
+	board.push_back(Region(16, "Great_Britain", vector<int>{13, 14, 17})); 
+	board.push_back(Region(17, "Northern_Europe", vector<int>{14, 15, 16, 18, 19}));
+	board.push_back(Region(18, "Western_Europe", vector<int>{16, 17, 19, 32})); 
+	board.push_back(Region(19, "Southern_Europe", vector<int>{15, 17, 18, 32, 33})); 
+	board.push_back(Region(20, "Ural", vector<int>{15, 21, 25, 26})); 
+	board.push_back(Region(21, "Siberia", vector<int>{20, 22, 23, 26, 27})); 
+	board.push_back(Region(22, "Yakutsk", vector<int>{21, 23, 24})); 
+	board.push_back(Region(23, "Irkutsk", vector<int>{21, 22, 24, 27})); 
+	board.push_back(Region(24, "Kamchatka", vector<int>{0, 22, 23, 27, 28}));
+	board.push_back(Region(25, "Afghanistan", vector<int>{15, 20, 26, 29, 30})); 
+	board.push_back(Region(26, "China", vector<int>{20, 25, 27, 30, 31})); 
+	board.push_back(Region(27, "Mongolia", vector<int>{21, 23, 24, 26, 28})); 
+	board.push_back(Region(28, "Japan", vector<int>{24, 27})); 
+	board.push_back(Region(29, "Middle_East", vector<int>{15, 19, 25, 30, 33, 34})); 
+	board.push_back(Region(30, "India", vector<int>{25, 26, 29, 31})); 
+	board.push_back(Region(31, "Southeast_Asia", vector<int>{26, 30, 38}));
+	board.push_back(Region(32, "North_Africa", vector<int>{10, 18, 19, 33, 34, 35})); 
+	board.push_back(Region(33, "Egypt", vector<int>{19, 29, 32, 34})); 
+	board.push_back(Region(34, "East_Africa", vector<int>{29, 32, 33, 35, 36, 37})); 
+	board.push_back(Region(35, "Central_Africa", vector<int>{32, 34, 36})); 
+	board.push_back(Region(36, "South_Africa", vector<int>{34, 35, 37})); 
+	board.push_back(Region(37, "Madagascar", vector<int>{34, 36}));
+	board.push_back(Region(38, "Indonesia", vector<int>{31, 39, 40})); 
+	board.push_back(Region(39, "New_Guinea", vector<int>{38, 41})); 
+	board.push_back(Region(40, "Western_Australia", vector<int>{38, 41})); 
+	board.push_back(Region(41, "Eastern_Australia", vector<int>{39, 40}));
 
-
-
+	// Initialize shuffled deck
+	initDeck("../Resources/card_deck.txt");
 }
 
 void Brisk::beginningClaim(vector<Player> players) {
@@ -88,18 +88,17 @@ void Brisk::beginningClaim(vector<Player> players) {
 	//find number of turns total after regions are chosen
 	switch (numPlayers)
 	{
-	case 2: numTurns = 40; break;
 	case 3: numTurns = 35; break;
 	case 4: numTurns = 30; break;
 	case 5: numTurns = 25; break;
 	}
 
-	regionsLeft = 42;
+	regionsLeft = NUM_REGIONS;
 
 	//claim new territories until territories gone
 
 	//go for the number of regions left to choose from
-	for (int i = 0; i < numTurns; i-=-1) {
+	for (int i = 0; i < numTurns; i++) {
 		//cycle through players
 		currentPlayer = (i % numPlayers);
 
@@ -122,7 +121,7 @@ void Brisk::beginningClaim(vector<Player> players) {
 			players[currentPlayer].updateArmySize(players[currentPlayer].getTotalArmySize() - 1);
 
 			//remove region left
-			regionsLeft -= 1;
+			regionsLeft--;
 		}
 		//else if there are no regions left
 		else {
